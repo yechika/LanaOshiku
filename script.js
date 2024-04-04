2097341ef1b4d9364103cb1ee250a3b61190fd57
DanielDirgantara <144701443+yechika@users.noreply.github.com>
2024-04-04 22:32:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,41 @@ $(document).ready(function() {
     var btn_open = $("#open");
     var btn_reset = $("#reset");
     var flap = $(".flap");
-
+    var motivationalText = $(".motivational-text"); 
+  
     btn_open.click(function() {
         open();
+        showMotivationalText(); 
     });
     btn_reset.click(function() {
         reset();
+        hideMotivationalText(); 
     });
-
+  
     function open() {
         $(".heart").css("opacity", "0");
         envelope.addClass("open").removeClass("close");
+        var video = document.getElementById('video');
+        video.play(); 
         setTimeout(function() {
             $(".heart").css("opacity", "1");
         }, 715); 
     }
-
+  
     function reset() {
         $(".heart").css("opacity", "0");
         envelope.addClass("close").removeClass("open");
         var video = document.getElementById('video');
-        video.pause();
+        video.pause(); 
         video.currentTime = 0; 
     }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var video = document.getElementById('video');
-    var playBtn = document.getElementById('open');
-
-    playBtn.addEventListener('click', function() {
-        video.play();
-    });
+  
+    
+    function showMotivationalText() {
+        motivationalText.removeClass("fade-out").addClass("fade-in").show(); 
+    }
+  
+    function hideMotivationalText() {
+        motivationalText.removeClass("fade-in").addClass("fade-out"); 
+    }
 });
